98cce5f0715c7562f5d256e63fee26b0608a2352
meferspb <mefer@mefer-HP-240-G8-Notebook-PC.com>
2023-05-26 18:53:09 +0500

    На ветке main  Ваша ветка опережает «origin/main» на 1 коммит.    (используйте «git push», чтобы опубликовать ваши локальные коммиты)
     Изменения, которые будут включены в коммит:
    	изменено:      install_kernel_on_device.sh
    	изменено:      make_kernel_lsdk_21.08.sh

--- make_kernel_lsdk_21.08.sh
+++ make_kernel_lsdk_21.08.sh
@@ -6,15 +6,19 @@ pause 10
 apt update && apt update
 apt install make libncurses-dev libncurses dwarves build-essential gcc bc bison flex libssl-dev libelf-dev
 ROOTDIR=`pwd`
-mkdir $ROOTDIR/build
-cd build
-git clone --depth 1 https://github.com/nxp-qoriq/linux -b LSDK-21.08
+
+mkdir -p $ROOTDIR/build
+if [[ ! -d $ROOTDIR/build/linux ]]; then
+        cd $ROOTDIR/build
+        git clone --depth 1 https://github.com/nxp-qoriq/linux -b LSDK-21.08
+        cd $ROOTDIR/build/linux
+        ./scripts/kconfig/merge_config.sh arch/arm64/configs/defconfig arch/arm64/configs/lsdk.config $ROOTDIR/configs/linux/lx2k_additions.config
+        git am $ROOTDIR/patches/linux-LSDK-21.08/*.patch
+fi
+
 cd $ROOTDIR/build/linux
-./scripts/kconfig/merge_config.sh arch/arm64/configs/defconfig arch/arm64/configs/lsdk.config $ROOTDIR/configs/linux/lx2k_additions.config
-git am $ROOTDIR/patches/linux-LSDK-21.08/*.patch
 make -j`nproc` all
 make -j`nproc` bindeb-pkg
 
 cp $ROOTDIR/build/linux/arch/arm64/boot/Image $ROOTDIR/build
-
 
